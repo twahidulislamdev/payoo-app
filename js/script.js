7c05d23b49334c11c991963459bd97b2fc732299
Twahidul Islam <twahidulislam2005@gmail.com>
2025-05-22 13:55:25 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.querySelector(".loginBtn").addEventListener("click", function (event) {
+  event.preventDefault();
+  const inputEmail = document.querySelector(".inputEmail").value;
+  const inputPassword = document.querySelector(".inputPassword").value;
+  // console.log(inputEmail, inputPassword);
+
+  if (inputEmail === "twahid746@gmail.com" && inputPassword === "123") {
+    console.log("Login Successfull");
+    window.location.href = "home.html"
+  } else {
+    alert("You did someThing Wrong");
+  }
+});
